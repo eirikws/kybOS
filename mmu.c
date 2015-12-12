c8a33d736484e108a2dd7ae4bdbc84f8ebb32a53
Eirik Solnør <woldsol@live.no>
2015-12-12 16:21:09 +0100

    cleanup

--- mmu.c
+++ mmu.c
@@ -30,7 +30,7 @@
 #define SECTION_ACCESS_PL1_RW_PL0_RO        ((2 << SECTION_AP01) | (0 << SECTION_AP2))
 #define SECTION_ACCESS_PL1_RW_PL0_RW        ((3 << SECTION_AP01) | (0 << SECTION_AP2))
 #define SECTION_ACCESS_PL1_RO_PL0_NONE      ((1 << SECTION_AP01) | (1 << SECTION_AP2))
-#define SECTION_ACCESS_PL1_RO_PL0_RO        ((3 << SECTION_AP01) | (1 << SECTION_AP2)) // reccomended
+#define SECTION_ACCESS_PL1_RO_PL0_RO        ((3 << SECTION_AP01) | (1 << SECTION_AP2))
 #define SECTION_EXECUTE_NEVER               (1 << SECTION_XN)
 #define SECTION_EXECUTE_ENABLE              (0 << SECTION_XN)
 #define SET_FORMAT_SECTION                  ((2 << 0) << (0 << 18))
@@ -41,7 +41,7 @@
 #define SECTION_OUT_IN_WR_THROUGH__NO_WRITE_ALOC  ((0 << SECTION_TEX) | (1 << SECTION_C) | (0 << SECTION_B))
 #define SECTION_OUT_IN_WR_BACK__NO_WRITE_ALOC   ((0 << SECTION_TEX) | (1 << SECTION_C) | (1 << SECTION_B))
 #define SECTION_OUT_INN_NON_CACHABLE    ((1 << SECTION_TEX) | (0 << SECTION_C) | (0 << SECTION_B))
-#define SECTION_OUT_INN_WRITE_BACK__WRITE_ALOC    ((1 << SECTION_TEX) | (1 << SECTION_C) | (0 << SECTION_B))
+#define SECTION_OUT_INN_WRITE_BACK__WRITE_ALOC    ((1 << SECTION_TEX) | (1 << SECTION_C) | (1 << SECTION_B))
 #define SECTION_DEVICE_NON_SHAREABLE    ((2 << SECTION_TEX) | (0 << SECTION_C) | (0 << SECTION_B))
 
 
@@ -50,83 +50,38 @@ static volatile __attribute__ ((aligned (0x4000))) uint32_t page_table[4096];
 void mmu_init_table(void) {
 	uint32_t base;
 	// initialize page_table
-	// 1024MB - 16MB of kernel memory (some belongs to the VC)
-	// default: 880 MB ARM ram, 128MB VC
+	// 1024MB - 16MB of kernel memory
+	// each page describes xx00000-xxFFFFF.
 	for (base = 0; base < 1024-16; base++) {
-	    // section descriptor (1 MB)
-	    // outer and inner write back, write allocate, shareable (fast but
-	    // unsafe)
-	    //uart_put_uint32_t(base << 20 | 0x1140E, 16);
-	    //uart_puts("\r\n");
-	    page_table[base] = base << 20 | 0x1140E;
-	    /*
 	    page_table[base] =    SET_FORMAT_SECTION
 	                          | base << SECTION_BASE_ADDRESS_OFFSET
 	                          | SECTION_SHAREABLE
-	                          | SECTION_STRONGLY_ORDERED
+	                          | SECTION_ACCESS_PL1_RW_PL0_RW
+	                          | SECTION_OUT_INN_WRITE_BACK__WRITE_ALOC
 	                          | SECTION_EXECUTE_ENABLE
 	                          | 0 << SECTION_DOMAIN // set the domain of this section to 0
 	                          | SECTION_GLOBAL
-	                          | SECTION_NON_SECURE;*/
+	                          | SECTION_NON_SECURE;
 	}
-    
-	// VC ram up to 0x3F000000
-//	for (base = 0; base < 1024 - 16; base++) {
-	    // section descriptor (1 MB)
-	    // outer and inner write through, no write allocate, shareable
-	    // page_table[base] = base << 20 | 0x1040A;
-	    // outer write back, write allocate
-	    // inner write through, no write allocate, shareable
-//	    page_table[base] = base << 20 | 0x1540A;
-//	}
-
-	// unused up to 0x3F000000
-//	for (; base < 1024 - 16; base++) {
-//	    page_table[base] = 0;
-//	}
-
-	// 16 MB peripherals at 0x3F000000
+	
+	// 16 MB peripherals at 0x3F000000-3ffffffff
 	for (; base < 1024; base++) {
-	    // shared device, never execute
-	    
-	    page_table[base] = base << 20 | 0x10416;
-	  /*  
 	    page_table[base] =      SET_FORMAT_SECTION
 	                          | base << SECTION_BASE_ADDRESS_OFFSET
 	                          | SECTION_SHAREABLE
+	                          | SECTION_ACCESS_PL1_RW_PL0_RW
 	                          | SECTION_DEVICE_SHAREABLE
 	                          | SECTION_EXECUTE_NEVER
 	                          | 0 << SECTION_DOMAIN
 	                          | SECTION_GLOBAL
-	                          | SECTION_NON_SECURE;*/
-	                          
-	                      
+	                          | SECTION_NON_SECURE;
 	}
-
-	// 1 MB mailboxes
-	// shared device, never execute
-	
-	//page_table[base] = base << 20 | 0x10416;
-	//++base;
-	
-	// unused up to 0x7FFFFFFF
-	for (; base < 2048; base++) {
-	    page_table[base] = 0;
-	}
-
-	// one second level page tabel (leaf table) at 0x80000000
-	//page_table[base++] = (intptr_t)leaf_table | 0x1;
-
-	// 2047MB unused (rest of address space)
+	// we have now reached 0x400000000 which is 1 GB, the size of RPI2 memory
+	// which means that the rest of memory should be invalid
 	for (; base < 4096; base++) {
 	    page_table[base] = 0;
 	}
-
-	// initialize leaf_table
-	//for (base = 0; base < 256; base++) {
-//	    leaf_table[base] = 0;
-//	}
-
+	return;
 }
 
 // ACTLR bits
@@ -162,19 +117,18 @@ void mmu_init_table(void) {
 #define TTB_INNER_WRITE_THROUGH_CACHEABLE               (0 << 0 | 1 << 6)
 #define TTB_INNER_WRITE_BACK__NO_WRITE_ALLOC_CACHEABLE  (1 << 1 | 1 << 6)
 
-
 // SCTLR bits
 #define THUMB_EXCEPTION_ENABLE                          (1 << 30)
 #define THUMB_EXCEPTION_DISABLE                         (0 << 30)
 #define ACCESS_FLAG_ENABLE                              (1 << 29)
 #define ACCESS_FLAG_DISABLE                             (0 << 29)
-#define TEX_REMAP_ENABLE                                (1 << 28
+#define TEX_REMAP_ENABLE                                (1 << 28)
 #define TEX_REMAP_DISABLE                               (0 << 28)
 #define NON_MASKABLE_FIQ_SUPPORT_OFFSET                 27
 #define EXCEPTION_ENDIANNESS_LITTLE                     (0 << 26)
 #define EXCEPTION_ENDIANNESS_BIT                        (1 << 26)
 #define INTERRUPT_VECTORS_USR_MADE                      (1 << 25)
-#define INTERRUPT_VECTORS_STD                           (0 << 25
+#define INTERRUPT_VECTORS_STD                           (0 << 25)
 #define FIQ_CONFIG_ENABLE_OFFSET                        21
 #define HARDWARE_ACCESS_FLAG_ENABLE                     (1 << 17)
 #define HARDWARE_ACCESS_FLAG_DISABLE                    (0 << 17)
@@ -205,22 +159,15 @@ void mmu_init(void) {
 	auxctrl |= 1 << SMP_ENABLE_COHERENT_REQUESTS;
 	asm volatile ("mcr p15, 0, %0, c1, c0,  1" :: "r" (auxctrl));
 
-    // setup domains (CP15 c3)
-	// Write Domain Access Control Register (DACTL
-    // use access permissions from TLB entry
-    // Set to 
+    // set domain 0 to be domain manager
     uint32_t dacr = (DOMAIN_MANAGER << 0);
 	asm volatile ("mcr     p15, 0, %0, c3, c0, 0" :: "r" (dacr));
 
-	// set domain 0 to client
-	//asm volatile ("mcr p15, 0, %0, c3, c0, 0" :: "r" (1));
-
-	// always use TTBR0
+	// configure TTBRC to always use TTBR0
 	// the reset value is the correct one, but it can't hurt to be safe!
 	asm volatile ("mcr p15, 0, %0, c2, c0, 2" :: "r" (0));
-
-	// set TTBR0 (page table walk inner and outer write-back,
-	// write-allocate, cacheable, shareable memory)
+    
+    // configure the TTBR0
 	uint32_t ttb_descr =       (unsigned)&page_table
 	                        |   TTB_SHAREABLE
 	                        |   TTB_REGION_OUT_WRITEBACK_WRITE_ALLOC_CACHEABLE
@@ -228,23 +175,31 @@ void mmu_init(void) {
 	
 	asm volatile ("mcr p15, 0, %0, c2, c0, 0"
 		      :: "r" (ttb_descr));
-	//	      :: "r" (0b1001010 | (unsigned) &page_table));
 	barrier_instruction();
 
-	//0b110001010000100001111000
+	//                 0b1100 0101 0000 1000 0111 1000
+	
+	// configure SCTLR, the system control register
+	// enabling the caches and branch prediction will result in a significant
+	// performance increase.
 	uint32_t mode;
-	asm volatile ("mrc p15, 0, %0, c1, c0, 0" : "=r" (mode));
-	// mask: 0b0111 0011 0000 0010 0111 1000 0010 0111
-	// bits: 0b0010 0000 0000 0000 0001 1000 0010 0111
-	uart_puts("SCTLR: ");
-	uart_put_uint32_t(mode, 2);
-	uart_puts("     ");
-	uart_put_uint32_t(mode, 16);
-	uart_puts("\r\n");
-	//delay(10000000);
-	mode &= 0x73027823;
-	mode |= 0x20000823;
+	
+	mode =      MMU_ENABLE
+	          | ALIGNMENT_CHECK_ENABLE
+	          | CACHE_DATA_ENABLE
+	          | BARRIERS_CP15_ENABLE
+	          | SWP_SWPB_ENABLE
+	          | BRANCH_PREDICTION_ENABLE
+	          | CACHE_INSTRUCTION_ENABLE
+	          | VECTORS_LOW
+	          | INTERRUPT_VECTORS_STD
+	          | CACHE_PLACEMENT_STRATEGY_PREDICTABLE
+	          | HARDWARE_ACCESS_FLAG_DISABLE
+	          | INTERRUPT_VECTORS_STD
+	          | EXCEPTION_ENDIANNESS_LITTLE
+	          | TEX_REMAP_DISABLE
+	          | ACCESS_FLAG_DISABLE
+	          | THUMB_EXCEPTION_DISABLE ;
+	          
 	asm volatile ("mcr p15, 0, %0, c1, c0, 0" :: "r" (mode) : "memory");
-
-
 }
